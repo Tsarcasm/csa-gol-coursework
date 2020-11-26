20ab30007b43b64ce1f39a16be75b4e8531c1969
Charles Lockie <lockiecharles@gmail.com>
2020-11-26 12:32:19 +0000

    Added benchmarking

--- gol/gol.go
+++ gol/gol.go
@@ -10,8 +10,6 @@ type Params struct {
 
 // Run starts the processing of Game of Life. It should initialise channels and goroutines.
 func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
-	// todo: handle keypresses
-
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
 	ioFilename := make(chan string)
--- gol_test.go
+++ gol_test.go
@@ -3,9 +3,37 @@ package main
 import (
 	"fmt"
 	"testing"
+
+	// "os"
 	"uk.ac.bris.cs/gameoflife/gol"
 	"uk.ac.bris.cs/gameoflife/util"
 )
+
+func benchmarkGol(b *testing.B) {
+	params := gol.Params{
+		ImageWidth:  512,
+		ImageHeight: 512,
+		Turns:       1000,
+		Threads:     64}
+	events := make(chan gol.Event)
+
+	gol.Run(params, events, nil)
+	for event := range events {
+		switch event.(type) {
+		case gol.FinalTurnComplete:
+			// print(e.Alive)
+		}
+	}
+}
+
+func BenchmarkGol(b *testing.B) {
+	// os.Stdout = nil // Disable all program output apart from benchmark results
+
+	// Run the test N times
+	for n := 0; n < b.N; n++ {
+		benchmarkGol(b)
+	}
+}
 
 // TestGol tests 16x16, 64x64 and 512x512 images on 0, 1 and 100 turns using 1-16 worker threads.
 func TestGol(t *testing.T) {
